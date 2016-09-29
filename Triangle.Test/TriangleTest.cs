8565dc821b328b1a99250dd3fd81a01bb87e7ccf
Anfalov <anfalovd10@yandex.ru>
2016-09-30 00:49:06 +0500

    The code is written.

---
+++ Triangle.Test/TriangleTest.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Triangle.Test
+{
+    [TestClass]
+    public class TriangleTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Side can't be less than 0.")]
+        public void Sides_Should_ThrowException_When_SideIsNegative()
+        {
+            Triangle.Sides(-5, 4, 8);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Sum of two sides can't be less than third side.")]
+        public void Sides_Should_ThrowException_When_TheRuleOfTriangleIsNotPerformed()
+        {
+            Triangle.Sides(2, 4, 8);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Angle must be between 0 and 180 degrees.")]
+        public void AngleAnd2Sides_Should_ThrowException_When_AngleIsGreaterThen180Degrees()
+        {
+            Triangle.AngleAnd2Sides(200, 3, 6);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Angle must be between 0 and 180 degrees.")]
+        public void AngleAnd2Sides_Should_ThrowException_When_AngleIsNegative()
+        {
+            Triangle.AngleAnd2Sides(-60, 3, 6);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Side can't be less than 0.")]
+        public void AngleAnd2Sides_Should_ThrowException_When_SideIsNegative()
+        {
+            Triangle.AngleAnd2Sides(60, -3, 6);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Side can't be less than 0.")]
+        public void SideAnd2Angles_Should_ThrowException_When_SideIsNegative()
+        {
+            Triangle.SideAnd2Angles(-5, 60, 30);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Angle can't be less than 0.")]
+        public void SideAnd2Angles_Should_ThrowException_When_AngleIsNegative()
+        {
+            Triangle.SideAnd2Angles(5, -60, 30);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Sum of two angles can't be more than 180 degrees.")]
+        public void SideAnd2Angles_Should_ThrowException_When_SumOfAnglesIsGreaterThen180Degrees()
+        {
+            Triangle.SideAnd2Angles(5, 150, 40);
+        }
+        [TestMethod]
+        public void Area_When_TriangleIsCreated_By_Sides()
+        {
+            double sideA = 87, sideB = 87, sideC = 126;
+            Triangle triangle = Triangle.Sides(sideA, sideB, sideC);
+            double area = triangle.Area();
+            Assert.AreEqual(3780, area, 1e-10);
+        }
+        [TestMethod]
+        public void Area_When_TriangleIsCreated_By_AngleAnd2Sides()
+        {
+            double angle = 30, sideA = 40, sideB = 20;
+            Triangle triangle = Triangle.AngleAnd2Sides(angle, sideA, sideB);
+            double area = triangle.Area();
+            Assert.AreEqual(200, area, 1e-10);
+        }
+        [TestMethod]
+        public void Area_When_TriangleIsCreated_By_SideAnd2Angles()
+        {
+            double side = 24, angleA = 150, angleB = 15;
+            Triangle triangle = Triangle.SideAnd2Angles(side, angleA, angleB);
+            double area = triangle.Area();
+            Assert.AreEqual(144, area, 1e-10);
+        }
+    }
+}
---
+++ Triangle/Triangle.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle
+{
+    public class Triangle
+    {
+        double a, b, c;
+        Triangle(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        public static Triangle Sides(double sideA, double sideB, double sideC)
+        {
+            if (sideA <= 0 || sideB <= 0 || sideC <= 0)
+                throw new ArgumentException("Side can't be less than 0.");
+            if (sideA + sideB <= sideC || sideA + sideC <= sideB || sideB + sideC <= sideA)
+                throw new ArgumentException("Sum of two sides can't be less than third side.");
+            return new Triangle(sideA, sideB, sideC);
+        }
+        public static Triangle AngleAnd2Sides(double angleA, double sideB, double sideC)
+        {
+            if (sideB <= 0 || sideC <= 0)
+                throw new ArgumentException("Side can't be less than 0.");
+            if (angleA <= 0 || angleA >= 180)
+                throw new ArgumentException("Angle must be between 0 and 180 degrees.");
+            double sideA = Math.Sqrt(Math.Pow(sideB, 2) + Math.Pow(sideC, 2) - 2 * sideB * sideC * Math.Cos(angleA * Math.PI / 180));
+            return new Triangle(sideA, sideB, sideC);
+        }
+        public static Triangle SideAnd2Angles(double sideA, double angleB, double angleC)
+        {
+            if (sideA <= 0)
+                throw new ArgumentException("Side can't be less than 0.");
+            if (angleB <= 0 || angleC <= 0)
+                throw new ArgumentException("Angle can't be less than 0.");
+            if (angleB + angleC >= 180)
+                throw new ArgumentException("Sum of two angles can't be more than 180 degrees.");
+            double angleA = 180 - (angleB + angleC);
+            angleA *= Math.PI / 180;
+            angleB *= Math.PI / 180;
+            angleC *= Math.PI / 180;
+            return new Triangle(sideA, sideA * Math.Sin(angleB) / Math.Sin(angleA), sideA * Math.Sin(angleC) / Math.Sin(angleA));
+        }
+        public double Area()
+        {
+            double p = (a + b + c) / 2;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+    }
+}
